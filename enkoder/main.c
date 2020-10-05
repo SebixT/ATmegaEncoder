250cbfe8f5426a5e703224d241a70cb9a3897179
SebixT <sebastian.tomalas@gmail.com>
2020-10-05 23:24:55 +0200

    direction detect. Channel a - interrupt, channel b - state check

--- enkoder/main.c
+++ enkoder/main.c
@@ -9,16 +9,44 @@
 #define delay_ms_1Mhz(x) (_delay_ms(x))   // Delay when prescaller set to 8x
 #define delay_ms_16Mhz(x) (_delay_ms(x*16)) // Delay when prescaller set to 1x so we need to burn 8x as many cycles
 
-#define LED (1<<PB5)
+#define CHANNEL_A PD2 // interrupt 0
+#define CHANNEL_B PD4 //
+#define LED		  PB5
+
+//#define SENSOR PD3
+volatile uint8_t pulseCount = 0;
+volatile uint8_t dir = 0; // 0 - right, 1 - left
+
 
 int main()
 {
-	DDRB |= LED;
+	DDRB = 0x00;
+	DDRD = 0x00;
+	DDRB |=  (1<<LED);
+	DDRD &=~ (1<<CHANNEL_A)|(1<<CHANNEL_B);
 	
+	EIMSK = 0b00000011;
 	
+	// ------------------------EICRA-----------------------------
+	// BIT 7 ---- 6 ---- 5 ---- 4 ---- 3 ---- 2 ---- 1 ---- 0 ---
+	// --- - ---- - ---- - ---- - ---ISC11--ISC10--ISC01--ISC00 -
+	// ------------------------------INTEREUPT 1---INTERRUPT 0---
+	//EICRA = 0b00000101; // Logical change
+	EICRA = 0b00001111; // Rising edge 
+	
+	sei();
 	while(1)
 	{
-		PORTB ^= LED;
-		delay_ms_1Mhz(1000);
+		
+	}
+}
+ISR(INT0_vect)
+{
+	if(!(PIND & (1<<CHANNEL_B))) {
+		PORTB |= (1<<LED);
+		dir = 0;
+	}else {
+		PORTB &=~ (1<<LED);
+		dir = 1;
 	}
 }
